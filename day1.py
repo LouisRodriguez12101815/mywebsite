78cae2c8f5c57d385da9b0913a3ae05de1587345
louis rodriguez <125697808+LouisRodriguez12101815@users.noreply.github.com>
2024-05-07 18:50:56 -0400

    Create day1.py
    day one of python class on coding

---
+++ day1.py
@@ -0,0 +1,4 @@
+first = int(input("Enter the first number: "))
+second int(input("enter the second number: "))
+#demonstrates in the print statement how you cannot concatenate strings 
+print ("The sum is", first+second)
